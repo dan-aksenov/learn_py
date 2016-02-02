a4a1620d1493475f4a0566266e392df7863274e7
dan-aksenov <dan-aksenov@yandex.ru>
2016-02-02 16:36:08 +0300

    hm.py
    more hostmon list parsings

--- hm.py
+++ hm.py
@@ -1,18 +1,12 @@
 import re
 
-def group_by_heading( some_source ):
-	buffer = []
-	for line in some_source:
-		if re.match("(.*)(Test #)(.*)", line):
-			if buffer: yield buffer
-			buffer =  [ re.findall("[0-9]+",line) ] 
-		else:
-			buffer.append( line )
-	yield buffer
-
+#def group_by_heading( some_source ):
 with open('HMall.txt', "r") as source:
-	for heading_and_lines in group_by_heading( source ):
-		heading = heading_and_lines[0]
-		lines = heading_and_lines[1:]
-		print heading
-	#	print lines 
+	hm_list = {"Test": 0, "Method": 0}
+	for line in source:
+		if re.match("(.*)(Test #)(.*)", line):
+			hm_list["Test"] = re.findall("[0-9]+",line) 
+		if re.match("(.*)(=)(.*)", line):
+			if re.match("(Method)(.*)",line):
+				hm_list["Method"] = line
+		print hm_list
