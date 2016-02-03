139c2882cc90480fda0580837320e82872aeee78
dan-aksenov <dan-aksenov@yandex.ru>
2016-02-03 13:02:53 +0300

    hm
    more parsing learnings

--- hm.py
+++ hm.py
@@ -1,12 +1,26 @@
 import re
+import string
 
-#def group_by_heading( some_source ):
+test={"nmb":0,"ttl":'null',"mtd":'null',"mst":'null'}
+#print "number			method			master"
 with open('HMall.txt', "r") as source:
-	hm_list = {"Test": 0, "Method": 0}
 	for line in source:
 		if re.match("(.*)(Test #)(.*)", line):
-			hm_list["Test"] = re.findall("[0-9]+",line) 
-		if re.match("(.*)(=)(.*)", line):
-			if re.match("(Method)(.*)",line):
-				hm_list["Method"] = line
-		print hm_list
+			tnum = re.findall("[0-9]+",line)
+			test["nmb"] = str(tnum)[2:].split("'",1)[0]
+			#print tnum
+		if re.match('Title       =.*', line):
+			tttl = re.findall('=.*', line)
+			test["ttl"]=(str(tttl)[3:]).split("'",1)[0]
+			#print (str(tttl)[3:]).split("'",1)[0]
+		#if re.match("(.*)(=)(.*)", line):
+		if re.match("Method      =.*",line):
+			tmeth = re.findall("=.*",line)
+			test["mtd"]=str(tmeth)[3:].split("'",1)[0]
+			#print tmeth
+		if re.match("MasterTest-Alive =.*",line):
+			tmast = re.findall("=.*",line)
+			test["mst"]=str(tmast)[3:].split("'",1)[0]
+			#print tmast
+			print test.get("nmb")+ test.get("ttl") + test.get("mtd") + test.get("mst")
+			
