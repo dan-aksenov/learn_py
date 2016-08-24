e1e9d1503c2f3f78dd6e913308c3d1c9318fa24b
Mefman <dan-aksenov@yandex.ru>
2016-08-24 17:40:58 +0300

    ex47 added

---
+++ ex47/ex47/game.py
@@ -0,0 +1,13 @@
+class Room(object):
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.path = {}
+
+    def go(self, direction):
+        return self.paths.get(direction, Name)
+
+    def add_paths(self, paths):
+        self.paths.update(paths)
+
