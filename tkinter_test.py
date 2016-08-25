3ebe7312a6073a6742e46606ad660a22f57d3930
dan-aksenov <dan-aksenov@yandex.ru>
2016-08-25 12:35:16 +0300

    tkinter - more exercises

--- tkinter_test.py
+++ tkinter_test.py
@@ -1,5 +1,18 @@
 import Tkinter
+import tkMessageBox
+from Tkinter import *
 
 top = Tkinter.Tk()
 # Code to add widgets will go here...
+def helloCallBack():
+    tkMessageBox.showinfo( "Hello Python", "Hello World")
+B = Tkinter.Button(top, text = "Hello", command = helloCallBack)
+B.pack()
+
+L1 = Label(top, text = "User Name")
+L1.pack(side = LEFT)
+E1 = Entry(top, bd=5)
+
+E1.pack(side = RIGHT)
+
 top.mainloop()
