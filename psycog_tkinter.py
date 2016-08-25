5e3acd64e530b1ebd6976861e82ab13a079bfb30
dan-aksenov <dan-aksenov@yandex.ru>
2016-08-25 16:42:36 +0300

    psycog and tkinter data insert added

--- psycog_tkinter.py
+++ psycog_tkinter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Small script to show PostgreSQL and Pyscopg together
+# Inserting into PostgreSQL Database.
 #
 
 from Tkinter import *
@@ -13,14 +13,20 @@ except:
     print "I am unable to connect to the database"
 
 cur = conn.cursor()
-
-cur.execute("""select datname from pg_database""")
-
+cur.execute("""select c1,c2 from test1 limit 10""")
 rows = cur.fetchall()
-
 top = Tkinter.Tk()
+
+e = Entry(top)
+e.pack()
+
+def insertdb():
+   cur.execute("""insert into test1(c1) values (%s)""", e.get() )
+   conn.commit()
+b = Tkinter.Button(top, text='commit', command = insertdb)
+b.pack()
 w = Listbox(top)
-w.pack()
+w.pack(side = RIGHT)
 for row in rows:
     w.insert(END, row)
 top.mainloop()
