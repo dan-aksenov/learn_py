0e332175027c8d817638d21b96293b05160b13b5
dan-aksenov <dan-aksenov@yandex.ru>
2016-08-25 12:52:21 +0300

    tkinter more stuff: 	user input and buttons tested

--- tkinter_test.py
+++ tkinter_test.py
@@ -4,15 +4,15 @@ from Tkinter import *
 
 top = Tkinter.Tk()
 # Code to add widgets will go here...
-def helloCallBack():
-    tkMessageBox.showinfo( "Hello Python", "Hello World")
-B = Tkinter.Button(top, text = "Hello", command = helloCallBack)
-B.pack()
 
 L1 = Label(top, text = "User Name")
 L1.pack(side = LEFT)
 E1 = Entry(top, bd=5)
-
 E1.pack(side = RIGHT)
+
+def helloCallBack():
+    tkMessageBox.showinfo( "Hello Python", E1.get() )
+B = Tkinter.Button(top, text = "Hello", command = helloCallBack)
+B.pack()
 
 top.mainloop()
