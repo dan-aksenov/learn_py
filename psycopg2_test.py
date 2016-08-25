0ea4de5789e88a356586322b6d9e4b1e71e70239
dan-aksenov <dan-aksenov@yandex.ru>
2016-08-25 11:30:08 +0300

    psycopg2 and tkinter exercises added

---
+++ psycopg2_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2.4
+#
+# Small script to show PostgreSQL and Pyscopg together
+#
+
+import psycopg2
+
+try:
+    conn = psycopg2.connect("dbname='postgres' user='postgres'")
+except:
+    print "I am unable to connect to the database"
+
+cur = conn.cursor()
+
+cur.execute("""select datname from pg_database""")
+
+rows = cur.fetchall()
+
+print "\nShow me the databases:\n"
+for row in rows:
+    print "   ", row[0]
+
