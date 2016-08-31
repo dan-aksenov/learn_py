0d0c0cc6f85d26197ca19a931027c42dce2d8013
dan-aksenov <dan-aksenov@yandex.ru>
2016-08-31 18:22:38 +0300

    minor fixes

--- my_finance/bin/app.py
+++ my_finance/bin/app.py
@@ -6,7 +6,7 @@ urls = (
  '/hello', 'Index'
 )
 
-#app = web.application(urls, globals())
+app = web.application(urls, globals())
 
 try:
     conn = psycopg2.connect("dbname='my_finance' user='dbax' host='192.168.155.156' password='210482'")
@@ -17,22 +17,25 @@ cur = conn.cursor()
 cur.execute("""select account,round(sum(summ)) s from my_finance_tbls.my_transactions group by account order by s desc""")
 rows = cur.fetchall()
 
-pie_chart = pygal.Pie()
-for row in rows:
-   pie_chart.add('a', row[1])
+#pie_chart = pygal.Pie()
+#for row in rows:
+#   pie_chart.add('a', row[1])
 #pie_chart.render(is_unicode=True)
-pie_chart.render_to_file('/tmp/chart.svg')
+#pie_chart.render_to_file('/tmp/chart.svg')
 
-#render = web.template.render('templates/')
 
-#class Index(object):
-#    def GET(self):
-#        return render.hello_form()
-#
-#    def POST(self):
-#        form = web.input(name="Nobody",greet="Hello")
-#        greeting = "%s, %s" % (form.greet, form.name)
-#        return render.index(greeting = greeting)
+for row in rows:
+   print " ", row[0], row[1]
+render = web.template.render('templates/')
 
-#if __name__ == "__main__":
-#    app.run()
+class Index(object):
+    def GET(self):
+        return render.hello_form()
+
+    def POST(self):
+       #form = web.input(name="Nobody",greet="Hello")
+       #greeting = "%s, %s" % (form.greet, form.name)
+       return render.index
+
+if __name__ == "__main__":
+    app.run()
