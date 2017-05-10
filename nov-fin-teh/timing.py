9891b0614230351d23025d7675dbe98328ff9160
dan-aksenov <dan-aksenov@yandex.ru>
2017-05-10 11:42:26 +0300

    time parameter added, sql file wirte refined

--- nov-fin-teh/timing.py
+++ nov-fin-teh/timing.py
@@ -2,7 +2,7 @@ import time, numpy, timeit, sys
 from test import MyHTMLParser
 arr = []
 start = time.time()
-PERIOD_OF_TIME = sys.argv[1]
+PERIOD_OF_TIME = float(sys.argv[1])
 while True :
     t=timeit.timeit('MyHTMLParser("http://python.org")', setup='from test import MyHTMLParser', number=1)
     arr.append(t)
@@ -10,8 +10,8 @@ while True :
 print(len(arr))
 print(numpy.average(arr))
 print(numpy.sum(arr))
-f = open('links.txt', 'w')
+f = open('results.sql', 'w')
 for a in (arr):
-   sql = 'insert into results("script_type", "time") values ("script1", ' + a + ');'
-   f.write(sql)
+   sql = 'insert into results("script_type", "time") values ("script1", ' + str(a) + ');'
+   f.write(sql + "\n")
 f.close()
