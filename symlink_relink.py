55cfdf68bb9d228d76bf12c4b8e7943d4b2b61ec
dan-aksenov <dan-aksenov@yandex.ru>
2017-09-25 15:28:59 +0300

    debuging stuff removed.

--- symlink_relink.py
+++ symlink_relink.py
@@ -4,11 +4,9 @@ import os
 # Path to restored tablespace folder.
 path = "/usr/local/pgsql/basebackup/db-09-22-2017_09/pg_tblspc/"
 print path
-input("Press Enter to continue...")
 # Get link names
 links = os.listdir( path )
 print links
-input("Press Enter to continue...")
 #['181171', '129717', '129725', '129718', '129719', '181170', '129723', '129720', '129722', '181172', '129724', '181169', '129721']
 
 # Rename every link in loop.
@@ -18,7 +16,6 @@ for link in links:
    #fullpath = os.path.abspath(link)
    fullpath = path + link
    print fullpath
-   input("Press Enter to continue...")
    # Validation if object is link can be added in future.
    #os.path.islink( fullpath )
    #True
@@ -26,15 +23,12 @@ for link in links:
    # Get existing link destination, which needs to be ranamed.   
    old_link_dest = os.path.realpath( fullpath )	
    print old_link_dest
-   input("Press Enter to continue...")
    #'/usr/local/pgsql/basebackup/tablespace/fdc_ods_geo_ind'
    # Set new link destination.
    new_link_dest = old_link_dest.replace( '/usr/local/pgsql/tablespace','/usr/local/pgsql/basebackup/tablespace' )
    print new_link_dest
-   input("Press Enter to continue...")
    #'/usr/local/pgsql/data/pg_tblcpc/tablespace/fdc_ods_geo_ind'
    # Recreate link.
    if os.path.lexists(fullpath):
       os.remove(fullpath)
    os.symlink( new_link_dest, fullpath )
-   input("Press Enter to continue...")
