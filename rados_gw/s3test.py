9319e657b15ed6ba9912db2833ce611f5ec11daa
dan-aksenov <dan-aksenov@yandex.ru>
2017-11-21 12:50:33 +0300

    fixes

--- rados_gw/s3test.py
+++ rados_gw/s3test.py
@@ -11,34 +11,33 @@ from boto.s3.key import Key
 
 # Got from http://docs.ceph.com/docs/master/install/install-ceph-gateway
 def s3connect( config_file ):
-   "Connect to given s3 gateway."
-   # Read gateway name and access keys from json conf file.
-   try:
-      with open( config_file ) as config_file:    
-         conn_data = json.load(config_file)
-   except:
-      print "Error: Config file does not exist."
-      return 0
+    "Connect to given s3 gateway using config file."
+    try:
+        with open( config_file ) as config_file:    
+            conn_data = json.load(config_file)
+    except:
+        print "Error: Unable to read config file. "
+        return 1
 
-   s3gw = conn_data['gateway']   
-   access_key = conn_data['access_key']
-   secret_key = conn_data['secret_key']
-   global conn
-   conn = boto.connect_s3(
-      aws_access_key_id=access_key,
-      aws_secret_access_key=secret_key,
-      host=s3gw, port=7480,                                                      	
-      is_secure=False, calling_format=boto.s3.connection.OrdinaryCallingFormat(),
-      )
-   return conn; 
+    s3gw = conn_data['gateway']   
+    access_key = conn_data['access_key']
+    secret_key = conn_data['secret_key']
+    global conn
+    conn = boto.connect_s3(
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+        host=s3gw, port=7480,                                                      	
+        is_secure=False, calling_format=boto.s3.connection.OrdinaryCallingFormat(),
+        )
+    return conn; 
 
 def buck_list():
-   "Get avaliable buckets."
-   print "Available buckets are:"
-   for bucket in conn.get_all_buckets():
-       print "{name} {created}".format(
-           name=bucket.name,
-           created=bucket.creation_date,
+    "Get avaliable buckets."
+    print "Available buckets are:"
+    for bucket in conn.get_all_buckets():
+        print "{name} {created}".format(
+            name=bucket.name,
+            created=bucket.creation_date,
        )
 
 def buck_add( buck_name ):
@@ -48,19 +47,19 @@ def buck_add( buck_name ):
 
 # Got from https://stackoverflow.com/questions/15085864/how-to-upload-a-file-to-directory-in-s3-bucket-using-boto
 def put_file( buck_name, file_name ):
-   "Insert file to bucket. Create bucket if not exists."
-   buck = buck = conn.create_bucket( buck_name )
-   print 'Uploading %s to bucket %s' % \
-   (file_name, buck)
+    "Insert file to bucket. Create bucket if not exists."
+    buck = conn.create_bucket( buck_name )
+    print 'Uploading %s to bucket %s' % \
+    (file_name, buck)
 
-   def percent_cb(complete, total):
-      sys.stdout.write('.')
-      sys.stdout.flush()
+    def percent_cb(complete, total):
+        sys.stdout.write('.')
+        sys.stdout.flush()
 
-   k = Key(buck)
-   k.key = 'my_test_file.txt'
-   k.set_contents_from_filename(file_name,
-      cb=percent_cb, num_cb=10)
+    k = Key(buck)
+    k.key = 'my_test_file.txt'
+    k.set_contents_from_filename(file_name,
+        cb=percent_cb, num_cb=10)
 
 # Got from http://docs.ceph.com/docs/master/radosgw/s3/python/
 def buck_cont( buck_name ):
