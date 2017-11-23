8b911defc247a1ca4c7e5ed3c597c10f1e27181c
dan-aksenov <dan-aksenov@yandex.ru>
2017-11-23 18:22:21 +0300

    more ema!

--- moex/ema.sql
+++ moex/ema.sql
@@ -19,3 +19,20 @@ ema as (
 )
 select dt, raw_fi, sales_ema
 from ema;	
+
+--more https://stackoverflow.com/questions/8871426/how-to-calculate-an-exponential-moving-average-on-postgres
+create or replace function ema_func(state numeric, inval float, alpha numeric)
+  returns numeric
+  language plpgsql as $$
+begin
+  return case
+         when state is null then inval
+         else alpha * inval + (1-alpha) * state
+         end;
+end
+$$;
+
+create aggregate ema(float, numeric) (sfunc = ema_func, stype = numeric);
+
+select dt,close,ema(close, 0.5) over (partition by ticker order by dt asc) from stock_hi
+st where ticker = 'SBER'
