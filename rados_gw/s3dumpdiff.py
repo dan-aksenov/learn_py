152abe5d7aefd41d6133a15106b6256797658c14
dan-aksenov <dan-aksenov@yandex.ru>
2017-12-06 13:07:37 +0300

    rgs to separate repo

--- rados_gw/s3dumpdiff.py
+++
@@ -1,8 +0,0 @@
-from s3client import s3connect, buck_dump_diff
-
-def main():
-    s3connect( config_file = raw_input("Config file: ") )
-    buck_dump_diff( buck_name = raw_input("Bucket: "), dump_path = raw_input("Dump path: ") )
-
-if __name__ == '__main__':
-    main()
--- rados_gw/s3list.py
+++
@@ -1,8 +0,0 @@
-from s3client import s3connect, buck_list
-
-def main():
-    s3connect( config_file = raw_input("Config file: ") )
-    buck_list() 
-
-if __name__ == '__main__':
-    main()
