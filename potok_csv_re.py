cb2ea215de95c32926c595e626ad68927454874b
dan-aksenov <dan-aksenov@yandex.ru>
2018-03-20 17:17:29 +0300

    dealing with floats

--- potok_csv_re.py
+++ potok_csv_re.py
@@ -24,25 +24,37 @@ interacc_pattern = re.compile('Внутрибанковский')
 nominal_pattern = re.compile('^Пополнение номинального')
 errors_pattern = re.compile('Возврат ошибочно переведенных средств по займам')
 
+def float_convert( input ):
+    output = float(input.replace(',', '.'))
+	return output
+
 for row in b:
     if returned_pattern.match(row[1]):
-        returned.append(row[1])
+        #returned.append(row[1])
+		returned.append(float_convert(row[2]))
     elif pcts_pattern.match(row[1]):
-	    pcts.append(row[1])
+	    #pcts.append(row[1])
+		pcts.append(float_convert(row[2]))
 	elif fines_pattern.match(row[1]):
-	    fines.append(row[1])
+	    #fines.append(row[1])
+		fines.append(float_convert(row[2]))
 	elif interacc_pattern.match(row[1]):
-	    interacc.append(row[1])
+	    #interacc.append(row[1])
+		interacc.append(float_convert(row[2]))
     elif nominal_pattern.match(row[1]):
-	    nominal.append(row[1])
+	    #nominal.append(row[1])
+		nominal.append(float_convert(row[2]))
 	elif errors_pattern.match(row[1]):
-	    errors.append(row[1])
+	    #errors.append(row[1])
+		errors.append(float_convert(row[2]))
 	else: #not other_pattern.match(row[1]):
-	    other.append(row[1])
+	    #other.append(row[1])
+		other.append(float_convert(row[2]))
 		
-print len(returned)
-print len(pcts)
-print len(fines)
-print len(interacc)
-print len(other)
+print "returned: " + sum(returned)
+print "interest: " + sum(pcts)  
+print "fines " + sum(fines)
+print "in: "+ sum(interacc)
+print "errors: " + len(errors)
+print "others: " + len(other)
 
