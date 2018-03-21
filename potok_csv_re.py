82f7dac59059a46c9cbd05f62e095de74565857c
dan-aksenov <dan-aksenov@yandex.ru>
2018-03-21 11:42:26 +0300

    date filter added

--- potok_csv_re.py
+++ potok_csv_re.py
@@ -4,6 +4,7 @@ Parse movementList.csv with data from potok.digital
 
 import re
 import csv
+from datetime import datetime
 
 a = open('d:/tmp/movementList.csv', 'rb')
 b =csv.reader(a, delimiter=';', quotechar='|')
@@ -26,31 +27,35 @@ errors_pattern = re.compile('Возврат ошибочно переведен�
 
 def float_convert( input ):
     output = float(input.replace(',', '.'))
-	return output
+    return output
 
+dt_start = datetime.strptime('01.03.2018', '%d.%m.%Y')
+dt_end = datetime.strptime('22.03.2018', '%d.%m.%Y')
+    
 for row in b:
-    if returned_pattern.match(row[1]):
-        #returned.append(row[1])
-		returned.append(float_convert(row[2]))
-    elif pcts_pattern.match(row[1]):
-	    #pcts.append(row[1])
-		pcts.append(float_convert(row[2]))
-	elif fines_pattern.match(row[1]):
-	    #fines.append(row[1])
-		fines.append(float_convert(row[2]))
-	elif interacc_pattern.match(row[1]):
-	    #interacc.append(row[1])
-		interacc.append(float_convert(row[2]))
-    elif nominal_pattern.match(row[1]):
-	    #nominal.append(row[1])
-		nominal.append(float_convert(row[2]))
-	elif errors_pattern.match(row[1]):
-	    #errors.append(row[1])
-		errors.append(float_convert(row[2]))
-	else: #not other_pattern.match(row[1]):
-	    #other.append(row[1])
-		other.append(float_convert(row[2]))
-		
+    if datetime.strptime(row[0], '%d.%m.%Y')>dt_start and datetime.strptime(row[0], '%d.%m.%Y')< dt_end:
+        if returned_pattern.match(row[1]):
+            #returned.append(row[1])
+            returned.append(float_convert(row[2]))
+        elif pcts_pattern.match(row[1]):
+            #pcts.append(row[1])
+            pcts.append(float_convert(row[2]))
+        elif fines_pattern.match(row[1]):
+            #fines.append(row[1])
+            fines.append(float_convert(row[2]))
+        elif interacc_pattern.match(row[1]):
+            #interacc.append(row[1])
+            interacc.append(float_convert(row[2]))
+        elif nominal_pattern.match(row[1]):
+            #nominal.append(row[1])
+            nominal.append(float_convert(row[2]))
+        elif errors_pattern.match(row[1]):
+            #errors.append(row[1])
+            errors.append(float_convert(row[2]))
+        else: #not other_pattern.match(row[1]):
+            #other.append(row[1])
+            other.append(float_convert(row[2]))
+        
 print "returned: " + str(sum(returned))
 print "interest: " + str(sum(pcts))  
 print "fines " + str(sum(fines))
