382c0a5256406280d4e120eeeacadb6c20b79089
dan-aksenov <dan-aksenov@yandex.ru>
2018-03-21 17:20:27 +0300

    input dates added

--- potok_csv_re.py
+++ potok_csv_re.py
@@ -21,30 +21,34 @@ pcts_pattern = re.compile('проценты')
 fines_pattern = re.compile('пени')
 invested_pattern = re.compile('инвестирование')
 
+dt_start = datetime.strptime(raw_input('Enter start date: '), '%d.%m.%Y')
+dt_end = datetime.strptime(raw_input('Enter end date: '), '%d.%m.%Y')
+
 def float_convert( input ):
     output = float(input.replace(',', '.'))
     return output
 
-dt_start = datetime.strptime('01.03.2018', '%d.%m.%Y')
-dt_end = datetime.strptime('22.03.2018', '%d.%m.%Y')
-    
-for row in b:
-    if datetime.strptime(row[2], '%d.%m.%Y')>dt_start and datetime.strptime(row[2], '%d.%m.%Y')< dt_end:
-        if returned_pattern.match(row[4]):
-            returned.append(float_convert(row[3]))
-        elif pcts_pattern.match(row[4]):
-            pcts.append(float_convert(row[3]))
-        elif fines_pattern.match(row[4]):
-            fines.append(float_convert(row[3]))
-        elif invested_pattern.match(row[4]):
-            invested.append(float_convert(row[3]))
-        else:
-            other.append(float_convert(row[3]))
+def main():
+    for row in b:
+        if datetime.strptime(row[2], '%d.%m.%Y')>dt_start and datetime.strptime(row[2], '%d.%m.%Y')< dt_end:
+            if returned_pattern.match(row[4]):
+                returned.append(float_convert(row[3]))
+            elif pcts_pattern.match(row[4]):
+                pcts.append(float_convert(row[3]))
+            elif fines_pattern.match(row[4]):
+                fines.append(float_convert(row[3]))
+            elif invested_pattern.match(row[4]):
+                invested.append(float_convert(row[3]))
+            else:
+                other.append(float_convert(row[3]))
 
-print 'From ' +    dt_start.strftime('%m.%d.%Y')
-print 'To   ' + dt_end.strftime('%m.%d.%Y')
-print 'Invested     : ' + str(sum(invested)) + '\n'			
-print 'Returded dept: ' + str(sum(returned))
-print 'Interest     : ' + str(sum(pcts))  
-print 'Fines        : ' + str(sum(fines))
-print 'Returned ALL : ' + str(sum(pcts)+sum(fines)+sum(returned))
+    print 'From         :' +    dt_start.strftime('%m.%d.%Y')
+    print 'To           :' + dt_end.strftime('%m.%d.%Y')
+    print 'Invested     : ' + str(sum(invested)) + '\n'			
+    print 'Returded dept: ' + str(sum(returned))
+    print 'Interest     : ' + str(sum(pcts))  
+    print 'Fines        : ' + str(sum(fines))
+    print 'Returned ALL : ' + str(sum(pcts)+sum(fines)+sum(returned))
+
+if __name__ == '__main__':
+    main()
