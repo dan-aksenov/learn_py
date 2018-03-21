f472bb4e0adf0a11d0a92ff3e4c94208c6379624
dan-aksenov <dan-aksenov@yandex.ru>
2018-03-21 16:47:53 +0300

    changed to potoks data.

--- potok_csv_re.py
+++ potok_csv_re.py
@@ -1,29 +1,29 @@
 '''
-Parse movementList.csv with data from potok.digital
+Parse reports from potok.digital.
+Sould be parsing potok.digital not click's exports.
 '''
 
 import re
 import csv
 from datetime import datetime
 
-a = open('d:/tmp/movementList.csv', 'rb')
+a = open('d:/tmp/potok_6367_21_03_2018.csv', 'rb')
 b =csv.reader(a, delimiter=';', quotechar='|')
 
 returned = []
 pcts = []
 fines = []
 interacc = []
-nominal = []
+invest = []
 other = []
 errors = []
 
-returned_pattern = re.compile('\"*Частичный')
-pcts_pattern = re.compile('\"*(О|У)плата процентов')
-fines_pattern = re.compile('\"*(О|У)плата пени')
-interacc_pattern = re.compile('Внутрибанковский')
+returned_pattern = re.compile('выплата')
+pcts_pattern = re.compile('проценты')
+fines_pattern = re.compile('пени')
+#interacc_pattern = re.compile('Внутрибанковский')
 #other_pattern = re.compile('(\"*Частичный)|(\"*плата процентов)|(\"*плата пени)')
-nominal_pattern = re.compile('^Пополнение номинального')
-errors_pattern = re.compile('Возврат ошибочно переведенных средств по займам')
+invest_pattern = re.compile('инвестирование')
 
 def float_convert( input ):
     output = float(input.replace(',', '.'))
@@ -33,32 +33,26 @@ dt_start = datetime.strptime('01.03.2018', '%d.%m.%Y')
 dt_end = datetime.strptime('22.03.2018', '%d.%m.%Y')
     
 for row in b:
-    if datetime.strptime(row[0], '%d.%m.%Y')>dt_start and datetime.strptime(row[0], '%d.%m.%Y')< dt_end:
-        if returned_pattern.match(row[1]):
+    if datetime.strptime(row[2], '%d.%m.%Y')>dt_start and datetime.strptime(row[2], '%d.%m.%Y')< dt_end:
+        if returned_pattern.match(row[4]):
             #returned.append(row[1])
-            returned.append(float_convert(row[2]))
-        elif pcts_pattern.match(row[1]):
+            returned.append(float_convert(row[3]))
+        elif pcts_pattern.match(row[4]):
             #pcts.append(row[1])
-            pcts.append(float_convert(row[2]))
-        elif fines_pattern.match(row[1]):
+            pcts.append(float_convert(row[3]))
+        elif fines_pattern.match(row[4]):
             #fines.append(row[1])
-            fines.append(float_convert(row[2]))
-        elif interacc_pattern.match(row[1]):
-            #interacc.append(row[1])
-            interacc.append(float_convert(row[2]))
-        elif nominal_pattern.match(row[1]):
+            fines.append(float_convert(row[3]))
+        elif invest_pattern.match(row[4]):
             #nominal.append(row[1])
-            nominal.append(float_convert(row[2]))
-        elif errors_pattern.match(row[1]):
-            #errors.append(row[1])
-            errors.append(float_convert(row[2]))
+            invest.append(float_convert(row[3]))
         else: #not other_pattern.match(row[1]):
             #other.append(row[1])
-            other.append(float_convert(row[2]))
+            other.append(float_convert(row[3]))
         
-print "returned: " + str(sum(returned))
-print "interest: " + str(sum(pcts))  
-print "fines " + str(sum(fines))
+print str(sum(returned))
+print str(sum(pcts))  
+print str(sum(fines))
 #print "in: "+ str(sum(interacc))
 #print "errors: " + str(len(errors))
 #print "others: " + str(len(other))
