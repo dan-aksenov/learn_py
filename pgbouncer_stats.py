72382e02ef12c958845c28b7dcbbcea7456a8b30
dan-aksenov <dan-aksenov@yandex.ru>
2018-04-10 16:47:11 +0300

    fix

--- pgbouncer_stats.py
+++ pgbouncer_stats.py
@@ -3,7 +3,7 @@
 import psycopg2
 import psycopg2.extras
 
-conn = psycopg2.connect(database='pgbouncer', user=user, password=password, host=host, port=port)
+conn = psycopg2.connect('dbname= ''pgbouncer'' user=''postgres'' password=''A8eQQaUH4xqKxP'' host=''10.139.127.9'' port=''6432''')
 
 conn.set_isolation_level(0)
 cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
