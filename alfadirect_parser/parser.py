10fd0d07e0edf114235acc2fddda60627faf3b08
dbax <dan-aksenov@yandex.ru>
2023-07-26 16:21:57 +0300

    fix parser to new alfa format

--- alfadirect_parser/parser.py
+++ alfadirect_parser/parser.py
@@ -1,20 +1,14 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 
 from xml.etree.ElementTree import parse
 doc = parse('/tmp/alfa.xml').getroot()
 
-store_items = []
 all_items = []
 
-for i in doc.findall('money_moves/money_move'):
-    settlement_date = i.find("settlement_date").text
-    oper_type = i.find("oper_type").text
-    comment = i.find("comment").text
-    volume = i.find("volume").text
-    p_code = i.find("p_code").text
-    oper_group = i.find("oper_group").text
-    store_items = [ settlement_date, oper_type, comment, volume, p_code, oper_group ]
-    all_items.append(store_items)
+for i in doc.iter('{TaxAppendix}Details'):
+  all_items.append(i.attrib)
 
-xmlToDf = pd.DataFrame(all_items, columns=[
-  'settlement_date', 'oper_type', 'comment', 'volume', 'p_code', 'oper_group' ] )
+xmlToDf = pd.DataFrame(all_items)
+
+xmlToDf.to_csv('/tmp/alfa.csv',encoding='utf-8',index=False)
