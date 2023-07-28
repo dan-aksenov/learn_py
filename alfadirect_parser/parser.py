ca916cd609eae6c0b5370ad177015740c8ba9410
dbax <dan-aksenov@yandex.ru>
2023-07-28 14:10:25 +0300

    append to excel

--- alfadirect_parser/parser.py
+++ alfadirect_parser/parser.py
@@ -11,4 +11,8 @@ for i in doc.iter('{TaxAppendix}Details'):
 
 xmlToDf = pd.DataFrame(all_items)
 
-xmlToDf.to_excel('/tmp/alfa.xlsx')
+# xmlToDf.to_excel('/tmp/alfa.xlsx')
+excel_file = '/tmp/alfa.xlsx'
+
+with pd.ExcelWriter( excel_file, mode='a') as writer:  
+  xmlToDf.to_excel(writer, sheet_name='Sheet_name_3')
